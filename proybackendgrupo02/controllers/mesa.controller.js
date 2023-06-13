2534169ee5e73a5aaf878ba530ac9d8a1df6ae4c
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-06-13 01:18:52 -0300

    "Mesa con post de alta mesa"

--- proybackendgrupo02/controllers/mesa.controller.js
+++ proybackendgrupo02/controllers/mesa.controller.js
@@ -6,4 +6,20 @@ mesaCtrl.getMesa = async (req, res) => { //se define una funcion asincrona
     res.json(mesas);
 }
 
+mesaCtrl.createMesa = async (req, res) => {
+    var mesa = new Mesa(req.body);
+    try {
+        await mesa.save();
+        res.json({
+            'status': '1',
+            'msg': 'Mesa guardada.'
+        })
+    } catch (error) {
+        res.status(400).json({
+            'status': '0',
+            'msg': 'Error procesando operacion.'
+        })
+    }
+}
+
 module.exports = mesaCtrl;
--- proybackendgrupo02/routes/usuario.route.js
+++ proybackendgrupo02/routes/usuario.route.js
@@ -3,7 +3,7 @@ const usuarioCtrl = require('./../controllers/usuario.controller');
 //creamos el manejador de rutas
 const express = require('express');
 const router = express.Router();
-//definimos las rutas para la gestion de producto
+
 router.post('/', usuarioCtrl.createUsuario); //Dar de alta un Usuario
 router.get('/', usuarioCtrl.getUsuario); //Obtener todas los Usuario
 router.delete('/:id', usuarioCtrl.deleteUsuario); //Eliminar un Usuario
