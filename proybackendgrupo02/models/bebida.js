ccb85f11019aee000278e54d9ddabf2b101272ed
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-06-21 22:13:00 -0300

    "Clase Menu - Falta mostrar bebidas disponibles"

--- proybackendgrupo02/models/bebida.js
+++ proybackendgrupo02/models/bebida.js
@@ -4,6 +4,7 @@ const BebidaSchema = new Schema ({
     ingredientesBebida : {type:String, requiere:true},
     nombreBebida : {type:String, requiere:true},
     precioBebida : {type:Number, requiere:true},
-    tipoVasoBebida : {type:String, requiere:true},  
+    tipoVasoBebida : {type:String, requiere:true}, 
+    disponibilidadBebida : {type:Boolean, requiere:true},  
 })
 module.exports = mongoose.models.Bebida || mongoose.model('Bebida',BebidaSchema);
