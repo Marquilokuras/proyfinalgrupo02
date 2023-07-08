5737407c93f9700f23947a0c414ca6b8a0d48014
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-07-08 00:59:50 -0300

    "Generar Excel terminado"

--- proybackendgrupo02/index.js
+++ proybackendgrupo02/index.js
@@ -8,8 +8,8 @@ var app = express();
 
 //middlewares
 //app.use(express.json());
-app.use(cors({ origin: 'https://proyfinalgrupo02.vercel.app' }));
-//app.use(cors({ origin: '*' }));
+//app.use(cors({ origin: 'https://proyfinalgrupo02.vercel.app' }));
+app.use(cors({ origin: '*' }));
 
 // Configuración del límite de tamaño de carga útil a 10MB
 app.use(bodyParser.json({ limit: '10mb' }));
