c34f3cbdc5c6b2367a0b25034d9d01720fed4c32
Marcos Quinteros <marcos.quinteros2003@gmail.com>
2023-07-12 13:28:26 -0300

    Mas arreglos para el hosting

--- proybackendgrupo02/routes/usuario.route.js
+++ proybackendgrupo02/routes/usuario.route.js
@@ -4,12 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/',usuarioCtrl.createUsuario); 
-router.get('/',autCtrl.verifyToken,usuarioCtrl.getUsuario); 
-router.delete('/:id',autCtrl.verifyToken,usuarioCtrl.deleteUsuario);
-router.put('/:id',autCtrl.verifyToken,usuarioCtrl.editUsuario);
+router.get('/'/* ,autCtrl.verifyToken */,usuarioCtrl.getUsuario); 
+router.delete('/:id'/* ,autCtrl.verifyToken */,usuarioCtrl.deleteUsuario);
+router.put('/:id'/* ,autCtrl.verifyToken */,usuarioCtrl.editUsuario);
 router.get('/recuperarContrasena',usuarioCtrl.recuperarContrasena);
 
-router.get('/auth/autenticacion',autCtrl.verifyToken,usuarioCtrl.mostrarUsuariosLogeados);
+router.get('/auth/autenticacion'/* ,autCtrl.verifyToken */,usuarioCtrl.mostrarUsuariosLogeados);
 router.post('/login', usuarioCtrl.loginUsuario); 
 
 module.exports = router;
