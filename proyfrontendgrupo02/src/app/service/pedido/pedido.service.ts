5a4b7df84546fd8847ec5b5ccffd700bf1fd2bf1
C:\MARCOS\APU 3\LABORATORIO DE PROGRAMACION ORIENTADA A OBJETOS <marquitos.q.27@gmail.com>
2023-07-13 23:17:02 -0300

    "Pedido nro de pedido agregado todo funciona"

--- proyfrontendgrupo02/src/app/service/pedido/pedido.service.ts
+++ proyfrontendgrupo02/src/app/service/pedido/pedido.service.ts
@@ -10,11 +10,11 @@ export class PedidoService {
   hostBase: string;
 
   constructor(private _http: HttpClient) {
-    this.hostBase = "https://proygrupo02.onrender.com/api/pedido/";
-    //this.hostBase = "http://localhost:3000/api/pedido/";
+    //this.hostBase = "https://proygrupo02.onrender.com/api/pedido/";
+    this.hostBase = "http://localhost:3000/api/pedido/";
   }
 
-  public generarPedido(bebidasPedido: Array<any>, email: string | null, fechaPedido : string,nombrePromocion : Array<any>,totalPedido:number ): Observable<any> {
+  public generarPedido(bebidasPedido: Array<any>, email: string | null, fechaPedido : string,nombrePromocion : Array<any>,totalPedido:number,nroPedido : number ): Observable<any> {
     const httpOption = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
@@ -27,7 +27,8 @@ export class PedidoService {
       'emailUsuario': email,
       'fechaPedido': fechaPedido,
       'arrayPromo': nombrePromocion,
-      'totalPedido': totalPedido
+      'totalPedido': totalPedido,
+      'numeroPedido': nroPedido
     };
 
     return this._http.post(this.hostBase , body, httpOption);
